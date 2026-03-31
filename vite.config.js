94f3a5eb159b6de58e2cd697e8443d721b9e3d9f
rnrsr <richard.nurser@gmail.com>
2026-03-31 21:30:23 +0100

    Upgrade Svelte to 5

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    conditions: ['browser', 'import', 'module', 'default']
+  },
   server: {
     port: 5173,
     open: true
